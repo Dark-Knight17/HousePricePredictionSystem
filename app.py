307085f3c33a385894cc224060a414ee07f6e060
Dark-Knight17 <151334619+Dark-Knight17@users.noreply.github.com>
2026-01-20 19:54:26 +0100

    Update Flask app to use environment variables for port

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def predict():
         return render_template("index.html", error=error_message, original_input=request.form)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG") == "1")
